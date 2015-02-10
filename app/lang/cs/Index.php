136ee78393bc45d3cb6a7c2b692d64bf6e5a9f20
martin131 <martin@matuna.cz>
2015-02-10 01:32:20 +0100

    adding czech files

---
+++ app/lang/cs/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+	// Headings
+	'heading' 	=> 'Request Your Channel',
+	// Request Forum
+	'email'		=> 'Email address',
+	'name'		=> 'Nickname',
+	'cname'		=> 'Channel name',
+	'note'		=> 'Notes',
+	'submit'	=> 'Submit',
+	// Request Form Placeholders
+	'email_ph'	=> 'example@example.com',
+	'name_ph'	=> 'Nickname on TeamSpeak Server ?',
+	'cname_ph'	=> 'Your new channel name ?',
+	'note_ph'	=> 'Extra information or requests ? (Optional)',
+	// Alerts
+	'success_a' => '<strong>Request Sent! </strong>Your request has been sent, An admin will review your request ASAP!',
+	'error_field' => '<strong>Error! </strong>Please make sure you filled all the fileds marked in red! <br /> <small>if you keep getting this error please contact the server admin.</small>',
+	'error_limit' => '<strong>Error! </strong>You have reached the requests limit<br /> <small>if you keep getting this error please contact the server admin.</small>',
+	);
---
+++ app/lang/cs/Settings.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+	// Headings
+	'heading' 		=> 'Settings',
+	// Nav Links
+	'requests'		=> 'Requests',
+	'history'		=> 'History',
+	'settings'		=> 'Settings',
+	'logout'		=> 'Logout',
+	// Change Password
+	'oldPass'		=> 'Old Password',
+	'newPass'		=> 'New Password',
+	'newPassAgain'	=> 'Confirm New Password',
+	'change'		=> 'Change',
+	// Alerts
+	'failed'		=> 'Wrong Password!',
+	'fields'		=> 'All fields required!',	
+	'noMatchMsg'	=> 'The new password did not match',
+	'changedMsg'	=> 'Password has been changed!',
+	);
